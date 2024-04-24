9ac85c80ffdc1b4e324518af9b87308a9ed0fe9c
biplove subedi <subedi.biplove10@gmail.com>
2024-04-24 15:23:08 +0545

    TopNav completed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import TopNav from "./components/TopNav.js";
+
 function App() {
   return (
    <>
-
+<div className="App">
+  <TopNav/>
+</div>
    </>
   );
 }
--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -1,9 +1,43 @@
-import React from 'react'
-
-function TopNav() {
+import React from "react";
+import {
+  AiOutlineMenu,
+  AiOutlineSearch,
+  AiOutlineClose,
+  AiFillTag,
+} from "react-icons/ai";
+import { BsFillCartFill, BsPerson } from "react-icons/bs";
+const TopNav = () => {
   return (
-    <div>TopNav</div>
-  )
-}
+    <div className="max-w-[1520] mx-auto flex justify-between items-center p-4">
+      <div className="flex items-center">
+        <div className="cursor-pointer">
+          <AiOutlineMenu size={25} />
+        </div>
+        <h1 className="text-2xl sm:text-4xl lg:text-3xl px-2">
+          Yum
+          <span>Eats</span>
+        </h1>
+        <div className=" lg:flex hidden items-center bg-gray-200 rounded-3xl p-1 text-[14px]">
+          <p className="bg-orange-700 text-white rounded-full p-2 text-bold">
+            Free{" "}
+          </p>
+          <p className=" p-2 text-bold">Delivery</p>
+        </div>
+      </div>
+      <div className="bg-gray-200 rounded-full flex align-center p-2 w-[200px] sm:w=[400px] lg:w-[500px]">
+        <AiOutlineSearch size={25} className="mx-1" />
+        <input
+          className="w-full bg-transparent focus:outline-none"
+          type="text"
+          placeholder="search meals ..."
+        />
+      </div>
+      <button className="bg-orange-700 text-white hidden md:flex lg:flex items-center py-2 rounded-full border-0">
+        <BsFillCartFill size={15} className="mx-1" />
+        Cart
+      </button>
+    </div>
+  );
+};
 
-export default TopNav
+export default TopNav;
